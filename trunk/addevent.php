30733cd99378ce956f47b989934a2d47fe18ffa7
work <work@12b15db6-9489-4049-b62a-67b714a0ed3f>
2008-09-14 02:34:12 +0000

    - main_upcoming_body.inc.php now handles DB errors. - addevent.php now handles DB errors.

--- trunk/addevent.php
+++ trunk/addevent.php
@@ -3,73 +3,106 @@ require_once('config.inc.php');
 require_once('session_start.inc.php');
 require_once('application.inc.php');
 
-  if (!authorized()) { exit; }
+if (!authorized()) { exit; }
 
-  if (isset($_POST['cancel'])) { setVar($cancel,$_POST['cancel'],'cancel'); } else { unset($cancel); }
-  if (isset($_POST['httpreferer'])) { setVar($httpreferer,$_POST['httpreferer'],'httpreferer'); } else { unset($httpreferer); }
-  if (isset($_GET['timebegin_year'])) { setVar($timebegin_year,$_GET['timebegin_year'],'timebegin_year'); } else { unset($timebegin_year); }
-  if (isset($_GET['timebegin_month'])) { setVar($timebegin_month,$_GET['timebegin_month'],'timebegin_month'); } else { unset($timebegin_month); }
-  if (isset($_GET['timebegin_day'])) { setVar($timebegin_day,$_GET['timebegin_day'],'timebegin_day'); } else { unset($timebegin_day); }
+if (isset($_POST['cancel'])) { setVar($cancel,$_POST['cancel'],'cancel'); } else { unset($cancel); }
+if (isset($_POST['httpreferer'])) { setVar($httpreferer,$_POST['httpreferer'],'httpreferer'); } else { unset($httpreferer); }
+if (isset($_GET['timebegin_year'])) { setVar($timebegin_year,$_GET['timebegin_year'],'timebegin_year'); } else { unset($timebegin_year); }
+if (isset($_GET['timebegin_month'])) { setVar($timebegin_month,$_GET['timebegin_month'],'timebegin_month'); } else { unset($timebegin_month); }
+if (isset($_GET['timebegin_day'])) { setVar($timebegin_day,$_GET['timebegin_day'],'timebegin_day'); } else { unset($timebegin_day); }
 
-  if (isset($_POST['cancel'])) {
-    redirect2URL("update.php");
-    exit;
-  };
+if (isset($_POST['cancel'])) {
+  redirect2URL("update.php");
+  exit;
+};
 
-  if (!isset($httpreferer)) { $httpreferer = $_SERVER["HTTP_REFERER"]; }
-	
-  // read sponsor name from DB
-  $result = DBQuery("SELECT name,url FROM vtcal_sponsor WHERE calendarid='".sqlescape($_SESSION["CALENDARID"])."' AND id='".sqlescape($_SESSION["AUTH_SPONSORID"])."'" ); 
-  $sponsor = $result->fetchRow(DB_FETCHMODE_ASSOC,0);
+if (!isset($httpreferer)) { $httpreferer = $_SERVER["HTTP_REFERER"]; }
 
-  // test if any template exists already
-  $result = DBQuery("SELECT * FROM vtcal_template WHERE calendarid='".sqlescape($_SESSION["CALENDARID"])."' AND sponsorid='".sqlescape($_SESSION["AUTH_SPONSORID"])."'" ); 
+// read sponsor name from DB
+$result =& DBQuery("SELECTx name,url FROM vtcal_sponsor WHERE calendarid='".sqlescape($_SESSION["CALENDARID"])."' AND id='".sqlescape($_SESSION["AUTH_SPONSORID"])."'" );
 
-  if ($result->numRows() == 0) {
-    // reroute to input page
-    $url = "changeeinfo.php?calendarid=".urlencode($_SESSION["CALENDARID"]);
-
-    // if addevent was called by clicking on the icons in week or month view provide the date info
-    if (isset($timebegin_year)) {
-      $url.="&templateid=0&timebegin_year=".$timebegin_year."&timebegin_month=".$timebegin_month."&timebegin_day=".$timebegin_day;
-    }
-    redirect2URL($url);
-    exit;
-  }
-
-  // print page header
+// Output an error message if the query failed.
+if (is_string($result)) {
   pageheader(lang('choose_template'), "");
   contentsection_begin(lang('choose_template'));
-?>
-<BR>
-<FORM method="post" action="changeeinfo.php">
-<?php
-  echo '<INPUT type="hidden" name="httpreferer" value="',$httpreferer,'">',"\n";
-?>
-  <SELECT name="templateid" size="6">
-    <OPTION selected value="0">----- <?php echo lang('blank'); ?> -----</OPTION>
-<?php
-  $result = DBQuery("SELECT * FROM vtcal_template WHERE calendarid='".sqlescape($_SESSION["CALENDARID"])."' AND sponsorid='".sqlescape($_SESSION["AUTH_SPONSORID"])."'" ); 
-  for ($i=0; $i<$result->numRows(); $i++) {
-    $template = $result->fetchRow(DB_FETCHMODE_ASSOC,$i);
-    echo "<OPTION value=\"",htmlentities($template['id']),"\">",htmlentities($template['name']),"</OPTION>\n";
+  DBErrorBox("Could not read sponsor name from DB: ".$result);
+	contentsection_end();
+	pagefooter();
+	DBclose();
+	exit();
+}
+
+$sponsor =& $result->fetchRow(DB_FETCHMODE_ASSOC,0);
+
+// test if any template exists already
+$result =& DBQuery("SELECT * FROM vtcal_template WHERE calendarid='".sqlescape($_SESSION["CALENDARID"])."' AND sponsorid='".sqlescape($_SESSION["AUTH_SPONSORID"])."'" );
+
+// Output an error message if the query failed.
+if (is_string($result)) {
+  pageheader(lang('choose_template'), "");
+  contentsection_begin(lang('choose_template'));
+  DBErrorBox("Could not test if template already exists: ".$result);
+	contentsection_end();
+	pagefooter();
+	DBclose();
+	exit();
+}
+
+if ($result->numRows() == 0) {
+  // reroute to input page
+  $url = "changeeinfo.php?calendarid=".urlencode($_SESSION["CALENDARID"]);
+
+  // if addevent was called by clicking on the icons in week or month view provide the date info
+  if (isset($timebegin_year)) {
+    $url.="&templateid=0&timebegin_year=".$timebegin_year."&timebegin_month=".$timebegin_month."&timebegin_day=".$timebegin_day;
   }
-?>
-  </SELECT>
-  <BR>
-  <BR>
-  <INPUT type="submit" name="choosetemplate" value="<?php echo lang('ok_button_text'); ?>">
-  <INPUT type="submit" name="cancel" value="<?php echo lang('cancel_button_text'); ?>">
-<?php
-  // forward date info, if the page was called with date info appended
-  // can later be done with PHP session management
-  if (isset($timebegin_year)) { echo "<INPUT type=\"hidden\" name=\"timebegin_year\" value=\"",$timebegin_year,"\">"; }
-  if (isset($timebegin_month)) { echo "<INPUT type=\"hidden\" name=\"timebegin_month\" value=\"",$timebegin_month,"\">"; }
-  if (isset($timebegin_day)) { echo "<INPUT type=\"hidden\" name=\"timebegin_day\" value=\"",$timebegin_day,"\">"; }
-?>
-</FORM>
-<?php
-  contentsection_end();
-  pagefooter();
+  redirect2URL($url);
+  exit;
+}
+
+// print page header
+pageheader(lang('choose_template'), "");
+contentsection_begin(lang('choose_template'));
+
+$result =& DBQuery("SELECT * FROM vtcal_template WHERE calendarid='".sqlescape($_SESSION["CALENDARID"])."' AND sponsorid='".sqlescape($_SESSION["AUTH_SPONSORID"])."'" ); 
+
+// Output an error message if $result is a string.
+if (is_string($result)) {
+	DBErrorBox($result);
+}
+
+// Otherwise, the query was successful.
+else {
+	?>
+	<BR>
+	<FORM method="post" action="changeeinfo.php">
+		<?php
+	  echo '<INPUT type="hidden" name="httpreferer" value="',$httpreferer,'">',"\n";
+		?>
+	  <SELECT name="templateid" size="6">
+	    <OPTION selected value="0">----- <?php echo lang('blank'); ?> -----</OPTION>
+		<?php
+	  for ($i=0; $i<$result->numRows(); $i++) {
+	    $template =& $result->fetchRow(DB_FETCHMODE_ASSOC,$i);
+	    echo "<OPTION value=\"",htmlentities($template['id']),"\">",htmlentities($template['name']),"</OPTION>\n";
+	  }
+		?>
+	  </SELECT>
+	  <BR>
+	  <BR>
+	  <INPUT type="submit" name="choosetemplate" value="<?php echo lang('ok_button_text'); ?>">
+	  <INPUT type="submit" name="cancel" value="<?php echo lang('cancel_button_text'); ?>">
+		<?php
+	  // forward date info, if the page was called with date info appended
+	  // can later be done with PHP session management
+	  if (isset($timebegin_year)) { echo "<INPUT type=\"hidden\" name=\"timebegin_year\" value=\"",$timebegin_year,"\">"; }
+	  if (isset($timebegin_month)) { echo "<INPUT type=\"hidden\" name=\"timebegin_month\" value=\"",$timebegin_month,"\">"; }
+	  if (isset($timebegin_day)) { echo "<INPUT type=\"hidden\" name=\"timebegin_day\" value=\"",$timebegin_day,"\">"; }
+		?>
+	</FORM>
+	<?php
+}
+contentsection_end();
+pagefooter();
 DBclose();
 ?>

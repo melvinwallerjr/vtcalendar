198e9ad93467870ea94762de46f47ab97fd472a1
amekkawi <amekkawi@12b15db6-9489-4049-b62a-67b714a0ed3f>
2008-09-25 21:26:49 +0000

    Bug fix for config-defaults.inc.php since you can't use empty() on constants.

--- trunk/config-defaults.inc.php
+++ trunk/config-defaults.inc.php
@@ -222,18 +222,18 @@ if (AUTH_LDAP && !function_exists("ldap_connect")) {
 }
 
 // Make sure the HTTP auth URL is specified if AUTH_HTTP is true.
-if (AUTH_HTTP && empty(AUTH_HTTP_URL)) {
+if (AUTH_HTTP && (!defined("AUTH_HTTP_URL") || AUTH_HTTP_URL == "")) {
 	echo "You must set AUTH_HTTP_URL if AUTH_HTTP is set to true.";
 	exit();
 }
 
 // Check that the required LDAP settings were set if AUTH_LDAP is true.
-if (AUTH_LDAP && (empty(LDAP_HOST) || empty(LDAP_USERFIELD) || empty(LDAP_BASE_DN))) {
+if (AUTH_LDAP && (!defined("LDAP_HOST") || LDAP_HOST == "" || !defined("LDAP_USERFIELD") || LDAP_USERFIELD == "" || !defined("LDAP_BASE_DN") || LDAP_BASE_DN == "")) {
 	echo "You must set LDAP_HOST, LDAP_USERFIELD and LDAP_BASE_DN if AUTH_LDAP is set to true.";
 	exit();
 }
 
-if (empty(BASEURL) || substr(BASEURL, -1) != "/") {
+if (!defined("BASEURL") || substr(BASEURL, -1) != "/") {
 	echo "You must set BASEURL and it MUST end with a slash ('/').";
 	exit();
 }

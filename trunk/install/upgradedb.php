fc6e737a2dfe37638374b89aa56cbaf418f209e9
amekkawi <amekkawi@12b15db6-9489-4049-b62a-67b714a0ed3f>
2008-09-26 20:26:06 +0000

    - Moved upgradedb.php functions to upgradedb-functions.php. - Removed some old fields that are no longer used. - Several changes to the graphical display of the upgradedb.php script.

--- trunk/install/upgradedb.php
+++ trunk/install/upgradedb.php
@@ -8,17 +8,44 @@ body {
 	font-family: Arial;
 	font-size: 13px;
 }
+form {
+	margin: 0;
+	padding: 0;
+}
+h1 {
+	font-size: 24px;
+	padding-bottom: 8px;
+	border-bottom: 2px solid #333333;
+}
+h2 {
+	font-size: 16px;
+	padding: 6px;
+	background-color: #CCDBFF;
+	border-top: 1px solid #666666;
+}
+h3.Table {
+	background-image: url(table-big.png);
+	background-repeat: no-repeat;
+	height: 24px;
+	padding: 0;
+	padding-left: 38px;
+	padding-top: 8px;
+	margin: 0;
+}
 div {
+	margin-bottom: 2px;
 	padding: 5px;
 }
-div.Success, div.Table, div.Field, div.PrimaryKey, div.Index {
+div.Error, div.Success, div.Table, div.Field, div.PrimaryKey, div.Index {
 	background-repeat: no-repeat;
 	background-position: 5px 5px;
 	padding-left: 30px;
 }
+div.Error {
+	background-image: url(failed.png);
+}
 div.Success {
 	background-image: url(success.png);
-	background-color: #DDDDFF;
 }
 div.Table {
 	background-image: url(table.png);
@@ -53,336 +80,67 @@ code {
 <body>
 <?php
 
-define("SQLLOGFILE", "");
-define("DATABASE", "mysql://root:@localhost/vtcalendar2-2-2");
-
-require_once("DB.php");
+@(include_once('DB.php')) or die('Pear::DB does not seem to be installed. See: http://pear.php.net/package/DB');
 require_once("../functions-db-generic.inc.php");
-
-$FinalSQL = "";
-
+require_once("upgradedb-functions.php");
 require_once("upgradedb-data.php");
 
-$CreateTable['vtcal_adminuser'] = "CREATE `vtcal_adminuser` ()";
-$CreateTable['vtcal_auth'] = "CREATE `vtcal_auth` ()";
-
-function GetTables() {
-	$TableData = array();
-	
-	$result =& DBquery("SHOW TABLES");
-	if (is_string($result)) {
-		echo "<div class='Error'><b>Error:</b> Failed to get tables: " . $result . "</div>";
-		return false;
-	}
-	else {
-		$count = $result->numRows();
-		
-		for ($i = 0; $i < $count; $i++) {
-			$record =& $result->fetchRow(DB_FETCHMODE_ASSOC, $i);
-			
-			if (!isset($keyname)) {
-				$keys = array_keys($record);
-				$keyname = $keys[0];
-			}
-			
-			GetTableData($TableData, $record[$keyname]);
-		}
-		$result->free();
-	}
-	
-	return $TableData;
+if (isset($_GET['DSN'])) {
+	define("DATABASE", $_GET['DSN']);
+}
+elseif (isset($_POST['DSN'])) {
+	define("DATABASE", $_POST['DSN']);
 }
 
-function GetTableData(&$TableData, $TableName) {
-	$TableData[$TableName] = array();
-	
-	$result =& DBquery("SHOW FIELDS FROM `" . $TableName . "`");
-	
-	if (is_string($result)) {
-		echo "<div class='Error'><b>Error:</b> Failed to get fields for `<code>" . $TableName . "</code>`: " . $result . "</div>";
-		return false;
-	}
-	else {
-		$TableData[$TableName]['Fields'] = array();
-		$TableData[$TableName]['Keys'] = array();
-		
-		$count = $result->numRows();
-		for ($i = 0; $i < $count; $i++) {
-			$record =& $result->fetchRow(DB_FETCHMODE_ASSOC, $i);
-			
-			$TableData[$TableName]['Fields'][$record['Field']]['Type'] = $record['Type'];
-			$TableData[$TableName]['Fields'][$record['Field']]['NotNull'] = strtolower($record['Null']) == "no";
-			$TableData[$TableName]['Fields'][$record['Field']]['AutoIncrement'] = strpos($record['Extra'],"auto_increment") !== false;
-		}
-		$result->free();
-	}
-	
-	$result =& DBquery("SHOW INDEXES FROM `" . $TableName . "`");
-	
-	if (is_string($result)) {
-		echo "<div class='Error'><b>Error:</b> Failed to get indexes for `<code>" . $TableName . "</code>`: " . $result . "</div>";
-		return false;
-	}
-	else {
-		$count = $result->numRows();
-		for ($i = 0; $i < $count; $i++) {
-			$record =& $result->fetchRow(DB_FETCHMODE_ASSOC, $i);
-			
-			$TableData[$TableName]['Keys'][$record['Key_name']]['Unique'] = !($record['Non_unique'] != 0);
-			$TableData[$TableName]['Keys'][$record['Key_name']]['Fields'][$record['Column_name']] = empty($record['Sub_part']) ? "" : $record['Sub_part'];
-		}
-		$result->free();
-	}
-}
+?>
+<h1>Upgrade VTCalendar MySQL DB</h1>
+<h2>Enter the Database Connection String:</h2>
+<blockquote>
+<form action="upgradedb.php" method="POST">
+	<p><b>Database Connection String:</b><br /> 
+    	<input name="DSN" type="text" id="DSN" size="60" value="<?php if (defined("DATABASE")) echo DATABASE; ?>" style="width: 600px;" /></p>
+	<p><input type="submit" value="Preview Database Upgrades" /></p>
+</form>
+</blockquote>
+<?php
 
-function CheckTables() {
-	global $CurrentTables, $FinalTables;
+if (defined("DATABASE")) {
+	?><h2>Upgrade Preview:</h2><?php
 	
-	$CurrentTableNames = array_keys($CurrentTables);
-	$FinalTableNames = array_keys($FinalTables);
+	$FinalSQL = "";
 	
-	// Loop through the current tables to see what can be dropped.
-	for ($i = 0; $i < count($CurrentTableNames); $i++) {
-		if (!array_key_exists($CurrentTableNames[$i], $FinalTables)) {
-			echo "<div class='Unused Table'><b>Unused Table Notice:</b> The `<code>" . $CurrentTableNames[$i] . "</code>` table is not used by VTCalendar (unless you've made moficiations).</div>";
-		}
+	$DBCONNECTION = DBOpen();
+	if (is_string($DBCONNECTION)) {
+		echo "<div class='Error'><b>Error:</b> Could not connect to the database: " . $DBCONNECTION . "</div>";
 	}
+	else {
 	
-	// Loop through the final tables to see what is missing.
-	for ($i = 0; $i < count($FinalTableNames); $i++) {
-		if (!array_key_exists($FinalTableNames[$i], $CurrentTables)) {
-			echo "<div class='Create Table'><b>Create Table:</b> The `<code>" . $FinalTableNames[$i] . "</code>` table is missing and will be created.</div>";
-			CreateTable($FinalTableNames[$i]);
+		$result =& DBquery("SELECT DATABASE() as SCHEMANAME");
+		if (is_string($result)) {
+			echo "<div class='Error'><b>Error:</b> Failed to determine schema name: " . $result . "</div>";
 		}
 		else {
-			CheckTable($FinalTableNames[$i]);
+			$record =& $result->fetchRow(DB_FETCHMODE_ASSOC, 0);
+			define("SCHEMA", $record['SCHEMANAME']);
+			$result->free();
+			
+			$CurrentTables =& GetTables();
+			CheckTables();
+			DBClose();
 		}
-	}
-}
-
-function CreateTable($TableName) {
-	global $FinalTables;
-
-	$sql = "";
-	
-	$FieldNames = array_keys($FinalTables[$TableName]['Fields']);
-	for ($i = 0; $i < count($FieldNames); $i++) {
-		if ($sql != "") {
-			$sql .= ",";
-		}
-		$sql .= "\n\t`" . $FieldNames[$i] . "` " . GetFieldSQL($FinalTables, $TableName, $FieldNames[$i]);
-	}
 		
-	$KeyNames = array_keys($FinalTables[$TableName]['Keys']);
-	for ($i = 0; $i < count($KeyNames); $i++) {
-		if ($sql != "") {
-			$sql .= ",";
-		}
-		if ($KeyNames[$i] == "PRIMARY") {
-			$sql .= "\n\t" . GetIndexFieldSQL($FinalTables, $TableName, $KeyNames[$i]);
+		?><h2>Upgrade Database:</h2><blockquote><?php
+		
+		if (empty($FinalSQL)) {
+			echo "<div class='Success'>No changes to the database were necessary.</div>";
 		}
 		else {
-			$sql .= "\n\t" . GetIndexFieldSQL($FinalTables, $TableName, $KeyNames[$i]);
-		}
-	}
-	
-	$sql = "CREATE TABLE `" . SCHEMA . "`.`" . $TableName . "` (" . $sql . "\n);\n\n";
-
-	$GLOBALS['FinalSQL'] .= $sql;
-}
-
-function CheckTable($TableName) {
-	global $CurrentTables, $FinalTables;
-	
-	$CurrentTableFields = array_keys($CurrentTables[$TableName]['Fields']);
-	$FinalTablesFields = array_keys($FinalTables[$TableName]['Fields']);
-	
-	$AlterFieldSQL = "";
-	
-	// Loop through the current table fields to see what can be dropped.
-	for ($i = 0; $i < count($CurrentTableFields); $i++) {
-		if (!array_key_exists($CurrentTableFields[$i], $FinalTables[$TableName]['Fields'])) {
-			echo "<div class='Unused Field'><b>Unused Field Notice:</b> The `<code>" . $CurrentTableFields[$i] . "</code>` field in the `<code>" . $TableName . "</code>` table is not used by VTCalendar (unless you've made moficiations).</div>";
-		}
-	}
-	
-	// Loop through the final table fields to see what is missing.
-	for ($i = 0; $i < count($FinalTablesFields); $i++) {
-		if (!array_key_exists($FinalTablesFields[$i], $CurrentTables[$TableName]['Fields'])) {
-			echo "<div class='Create Field'><b>New Field:</b> The `<code>" . $FinalTablesFields[$i] . "</code>` field in the `<code>" . $TableName . "</code>` table is missing and will be created as `<code>" . GetFieldSQL($FinalTables, $TableName, $FinalTablesFields[$i]) . "</code>`.</div>";
-			
-			if (!empty($AlterFieldSQL)) {
-				$AlterFieldSQL .= ", \n";
-			}
-			$AlterFieldSQL .= "\tADD COLUMN `" . $FinalTablesFields[$i] . "` " . GetFieldSQL($FinalTables, $TableName, $FinalTablesFields[$i]);
-			//TODO: Add 'AFTER `abccolumn`'
-		}
-		else {
-			
-			if (!CheckField($TableName, $FinalTablesFields[$i])) {
-				if (!empty($AlterFieldSQL)) {
-					$AlterFieldSQL .= ", \n";
-				}
-				$AlterFieldSQL .= "\tMODIFY COLUMN `" . $FinalTablesFields[$i] . "` " . GetFieldSQL($FinalTables, $TableName, $FinalTablesFields[$i]);
-			}
-		}
-	}
-	
-	$CurrentTableIndexes = array_keys($CurrentTables[$TableName]['Keys']);
-	$FinalTablesIndexes = array_keys($FinalTables[$TableName]['Keys']);
-	
-	// Loop through the current table indexes to see what can be dropped.
-	for ($i = 0; $i < count($CurrentTableIndexes); $i++) {
-		if (!array_key_exists($CurrentTableIndexes[$i], $FinalTables[$TableName]['Keys'])) {
-			echo "<div class='Unused Index'><b>Unused Index Notice:</b> The `<code>" . $CurrentTableIndexes[$i] . "</code>` index in the `<code>" . $TableName . "</code>` table is not specified by VTCalendar (unless you've made moficiations).</div>";
-		}
-	}
-	
-	// Loop through the final table indexes to see what is missing.
-	for ($i = 0; $i < count($FinalTablesIndexes); $i++) {
-		$addIndex = false;
-		$modifyIndex = false;
-		
-		// The index does not exist.
-		if (!array_key_exists($FinalTablesIndexes[$i], $CurrentTables[$TableName]['Keys'])) {
-			echo "<div class='Create " . ($FinalTablesIndexes[$i] == "PRIMARY" ? "PrimaryKey" : "Index") . "'><b>New " . ($FinalTablesIndexes[$i] == "PRIMARY" ? "Primary Key" : "Index") . ":</b> The " . ($FinalTablesIndexes[$i] == "PRIMARY" ? "primary key" : "`<code>" . $FinalTablesIndexes[$i] . "</code>` index") . " in the `<code>" . $TableName . "</code>` table is missing and will be created as `<code>" . GetIndexFieldSQL($FinalTables, $TableName, $FinalTablesIndexes[$i]) . "</code>`.</div>";
-			$addIndex = true;
+			?>
+			<textarea name="updatesql" cols="60" rows="15" readonly="readonly" onFocus="this.select();" onClick="this.select(); this.focus();"><?php echo htmlentities($FinalSQL); ?></textarea>
+			<?php
 		}
 		
-		// The index exists and needs to be changed.
-		elseif (!CheckIndex($TableName, $FinalTablesIndexes[$i])) {
-			$modifyIndex = true;
-		}
-		
-		// Drop and add the recreate the index if it needs to be added/changed.
-		if ($addIndex || $modifyIndex) {
-			if (!empty($AlterFieldSQL)) $AlterFieldSQL .= ", \n";
-			
-			$AlterFieldSQL .= "\t";
-			
-			if ($modifyIndex) {
-				// The PRIMARY KEY has a different syntax when being dropped.
-				if ($FinalTablesIndexes[$i] == "PRIMARY") {
-					$AlterFieldSQL .= "DROP PRIMARY KEY, ";
-				}
-				
-				// Normal synatx for dropping indexes.
-				else {
-					$AlterFieldSQL .= "DROP INDEX `" . $FinalTablesIndexes[$i] . "`, ";
-				}
-			}
-			
-			// Add the index.
-			$AlterFieldSQL .= "ADD " . GetIndexFieldSQL($FinalTables, $TableName, $FinalTablesIndexes[$i]);
-		}
-	}
-	
-	// Add the ALTER TABLE header to the SQL for this table, if there were any changes.
-	if ($AlterFieldSQL != "") {
-		$GLOBALS['FinalSQL'] .= "ALTER TABLE `" . SCHEMA . "`.`" . $TableName . "` \n" . $AlterFieldSQL . ";\n\n";
-	}
-	
-	return true;
-}
-
-function CheckField($TableName, $FieldName) {
-	global $CurrentTables, $FinalTables;
-	
-	$CurrentField = $CurrentTables[$TableName]['Fields'][$FieldName];
-	$FinalField = $FinalTables[$TableName]['Fields'][$FieldName];
-	
-	if ($CurrentField['Type'] != $FinalField['Type']
-		|| $CurrentField['NotNull'] != $FinalField['NotNull']
-		|| $CurrentField['AutoIncrement'] != $FinalField['AutoIncrement']) {
-		
-		echo "<div class='Alter Field'><b>Alter Field:</b> The `<code>" . $FieldName . "</code>` field in the `<code>" . $TableName . "</code>` table needs to be altered from `<code>" . GetFieldSQL($CurrentTables, $TableName, $FieldName) . "</code>` to `<code>" . GetFieldSQL($FinalTables, $TableName, $FieldName) . "</code>`.</div>";
-		return false;
-	}
-	return true;
-}
-
-function CheckIndex($TableName, $IndexName) {
-	global $CurrentTables, $FinalTables;
-	
-	$CurrentIndex = $CurrentTables[$TableName]['Keys'][$IndexName];
-	$FinalIndex = $FinalTables[$TableName]['Keys'][$IndexName];
-	
-	if (GetIndexFieldSQL($CurrentTables, $TableName, $IndexName) != GetIndexFieldSQL($FinalTables, $TableName, $IndexName)) {
-		
-		echo "<div class='Alter " . ($IndexName == "PRIMARY" ? "PrimaryKey" : "Index") . "'><b>Alter " . ($IndexName == "PRIMARY" ? "Primary Key" : "Index") . ":</b> The `<code>" . $IndexName . "</code>` index in the `<code>" . $TableName . "</code>` table needs to be altered from `<code>" . GetIndexFieldSQL($CurrentTables, $TableName, $IndexName) . "</code>` to `<code>" . GetIndexFieldSQL($FinalTables, $TableName, $IndexName) . "</code>`.</div>";
-		return false;
-	}
-	return true;
-}
-
-function CheckIndexFields($TableName, $IndexName) {
-	return true;
-}
-
-function GetFieldSQL(&$TableData, $TableName, $FieldName) {
-	$Field = $TableData[$TableName]['Fields'][$FieldName];
-	
-	$sql = $Field['Type'];
-	if ($Field['NotNull']) $sql .= " NOT NULL";
-	if ($Field['AutoIncrement']) $sql .= " auto_increment";
-	
-	return $sql;
-}
-
-function GetIndexFieldSQL(&$TableData, $TableName, $IndexName) {
-	$Index = $TableData[$TableName]['Keys'][$IndexName];
-	$IndexFields = array_keys($Index['Fields']);
-	
-	$sql = "";
-	for ($i = 0; $i < count($IndexFields); $i++) {
-		if ($i >= 1) {
-			$sql .= ", ";
-		}
-	
-		$subPart = $Index['Fields'][$IndexFields[$i]];
-		
-		$sql .= $IndexFields[$i];
-		if (!empty($subPart)) $sql .= "(" . $subPart . ")";
-	}
-	
-	if ($IndexName == 'PRIMARY') {
-		return "PRIMARY KEY (" . $sql . ")";
-	}
-	else {
-		if ($Index['Unique']) {
-			return "UNIQUE INDEX `" . $IndexName . "` (" . $sql . ")";
-		}
-		else {
-			return "INDEX `" . $IndexName . "` (" . $sql . ")";
-		}
-	}
-}
-
-$DBCONNECTION = DBOpen();
-if (is_string($DBCONNECTION)) {
-	echo "<div class='Error'><b>Error:</b> Could not connect to the database: " . $DBCONNECTION . "</div>";
-}
-else {
-
-	$result =& DBquery("SELECT DATABASE() as SCHEMANAME");
-	if (is_string($result)) {
-		echo "<div class='Error'><b>Error:</b> Failed to determine schema name: " . $result . "</div>";
-	}
-	else {
-		$record =& $result->fetchRow(DB_FETCHMODE_ASSOC, 0);
-		define("SCHEMA", $record['SCHEMANAME']);
-		$result->free();
-		
-		$CurrentTables =& GetTables();
-		CheckTables();
-		DBClose();
-	}
-	
-	if (empty($FinalSQL)) {
-		echo "<div class='Success'><b>Success:</b> No changes to the database were necessary.</div>";
-	}
-	else {
-		?><hr><pre><?php echo htmlentities($FinalSQL); ?></pre><?php
+		?></blockquote><?php
 	}
 }
 

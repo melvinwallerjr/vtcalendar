acb52fe62868d4c25c8ccdd430f4e421691d7465
amekkawi <amekkawi@12b15db6-9489-4049-b62a-67b714a0ed3f>
2008-09-26 21:37:06 +0000

    Improved various text in upgradedb.php and changed the way changes were handled so that a cleaner page can be displayed if no changes or notices were displayed.

--- install/upgradedb.php
+++ install/upgradedb.php
@@ -83,7 +83,7 @@ function verifyUpgrade() {
 </head>
 
 <body>
-<h1>Upgrade VTCalendar MySQL DB</h1>
+<h1>Install/Upgrade VTCalendar Database (MySQL Only)</h1>
 
 <?php
 
@@ -106,15 +106,17 @@ if (isset($_POST['updatesql'])) {
 ?>
 <h2>About this Page:</h2>
 <blockquote>
-<p>When upgrading VTCalendar, it is necessary to upgrade the database as well. This page will scan your current database and tell you what needs to be changed. You can then apply the changes to the database directly through this page, or copy/paste the necessary SQL into another program of your choice.</p>
-<p><b style="color: #CC0000;">Backup Your Database!</b><br/>It is recommended that you backup your entire VTCalendar database before upgrading. Changes made by applying this upgrade CANNOT be undone without a backup.</p>
+<p>If this is a <b>fresh VTCalendar install</b> this script will create the necessary VTCalendar tables.</p>
+<p>If you are <b>upgrading VTCalendar</b> it is necessary to upgrade the database as well. This page will scan your current database schema and tell you what needs to be changed. You can then apply the changes to the database directly through this page, or copy/paste the necessary SQL into another program of your choice.</p>
+<p><b style="color: #CC0000;">Backup Your Database!</b><br/>If you are upgrading VTCalendar it is recommended that you backup your entire VTCalendar database. Changes made by applying this upgrade CANNOT be undone without a backup.</p>
 </blockquote>
 
 <h2>Enter the Database Connection String:</h2>
 <blockquote>
 <form action="upgradedb.php" method="POST">
 	<p><b>Database Connection String:</b><br /> 
-    	<input name="DSN" type="text" id="DSN" size="60" value="<?php if (defined("DATABASE")) echo DATABASE; ?>" style="width: 600px;" /></p>
+    	<input name="DSN" type="text" id="DSN" size="60" value="<?php if (defined("DATABASE")) echo DATABASE; ?>" style="width: 600px;" /><br/>
+    	<i>Example: mysql://user:password@localhost/vtcalendar</i></p>
 	<p><input type="submit" value="Preview Database Upgrades" /></p>
 </form>
 </blockquote>
@@ -161,7 +163,7 @@ elseif (defined("DATABASE") && defined("UPGRADESQL")) {
 	}
 }
 elseif (defined("DATABASE") && !defined("UPGRADESQL")) {
-	?><h2>Upgrade Preview:</h2><p>The following is a preview of changes to the database that are needed.<br/>To apply any needed changes, proceed to the <a href="#Upgrade">Upgrade the Database</a> section at the bottom of this page.<?php
+	?><h2>Upgrade Preview:</h2><?php
 	
 	$FinalSQL = "";
 	
@@ -180,28 +182,38 @@ elseif (defined("DATABASE") && !defined("UPGRADESQL")) {
 			define("SCHEMA", $record['SCHEMANAME']);
 			$result->free();
 			
-			$CurrentTables =& GetTables();
-			CheckTables();
+			// Get the current table data.
+			if (($CurrentTables = GetTables()) !== false) {
+				
+				?><p>The following is a preview of changes to the database that are needed.<br/>To apply any needed changes, proceed to the <a href="#Upgrade">Upgrade the Database</a> section at the bottom of this page.</p><?php
+				
+				// Check the current table data vs the final table data.
+				$changes = CheckTables();
+		
+				?><h2><a name="Upgrade"></a>Upgrade Database:</h2><form action="upgradedb.php" method="post" onsubmit="return verifyUpgrade();"><input type="hidden" name="DSN" value="<?php echo DATABASE; ?>"/><blockquote><?php
+				
+				if ($changes < 1) {
+					echo "<div class='Success'>No changes to the database were necessary.</div>";
+					
+					// Show a cleaner success page if no changes or notifications were outputted.
+					if ($changes == 0) {
+						?><script type="text/javascript">location.replace("upgradedb.php?success=nochanges")</script><?php
+					}
+				}
+				else {
+					?>
+					<p style="font-size: 18px; padding: 8px; background-color: #FFEEEE; border: 1px solid #FF3333;"><b>LAST WARNING!</b><br/>If you are upgrading VTCalendar, it is recommended that you backup your entire VTCalendar database. Changes made by applying this upgrade CANNOT be undone without a backup.</p>
+					<p>After reviewing the above upgrades you may <input type="submit" value="Upgrade the Database"/></p>
+					<p style="font-size: 16px; font-weight: bold;">- or -</p>
+					<div>If your account does not have permission to CREATE or ALTER tables,<br/>copy/paste the SQL code below to manually upgrade your database</div>
+					<textarea name="updatesql" cols="60" rows="15" readonly="readonly" onFocus="this.select();" onClick="this.select(); this.focus();"><?php echo htmlentities($FinalSQL); ?></textarea>
+					<?php
+				}
+				
+				?></blockquote></form><?php
+			}
 			DBClose();
 		}
-		
-		?><h2><a name="Upgrade"></a>Upgrade Database:</h2><form action="upgradedb.php" method="post" onsubmit="return verifyUpgrade();"><input type="hidden" name="DSN" value="<?php echo DATABASE; ?>"/><blockquote><?php
-		
-		if (empty($FinalSQL)) {
-			echo "<div class='Success'>No changes to the database were necessary.</div>";
-			?><!--<script type="text/javascript">location.replace("upgradedb.php?success=nochanges")</script>--><?php
-		}
-		else {
-			?>
-			<p style="font-size: 18px; padding: 8px; background-color: #FFEEEE; border: 1px solid #FF3333;"><b>LAST WARNING!</b><br/>It is recommended that you backup your entire VTCalendar database before upgrading. Changes made by applying this upgrade CANNOT be undone without a backup.</p>
-			<p>After reviewing the above upgrades you may <input type="submit" value="Upgrade the Database"/></p>
-			<p style="font-size: 16px; font-weight: bold;">- or -</p>
-			<div>If your account does not have permission to CREATE or ALTER tables,<br/>copy/paste the SQL code below to manually upgrade your database</div>
-			<textarea name="updatesql" cols="60" rows="15" readonly="readonly" onFocus="this.select();" onClick="this.select(); this.focus();"><?php echo htmlentities($FinalSQL); ?></textarea>
-			<?php
-		}
-		
-		?></blockquote></form><?php
 	}
 }
 

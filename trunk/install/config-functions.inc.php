66f5eb62b8c5401fe0f4955b338b474077bfbc60
amekkawi <amekkawi@12b15db6-9489-4049-b62a-67b714a0ed3f>
2008-09-28 00:25:55 +0000

    AddMainAdmin and AddUser functions in install/config-functions.inc.php modified to return an error message instead of add it directly to the FormErrors array.

--- trunk/install/config-functions.inc.php
+++ trunk/install/config-functions.inc.php
@@ -21,49 +21,60 @@ function ProcessMainAdminAccountList($accountlist, &$finallist) {
 }
 
 function AddMainAdmin($username) {
-	global $FormErrors;
-	
 	// Check that the account does not already exist
 	$result =& DBQuery("SELECT count(*) as idcount FROM vtcal_adminuser WHERE (id='".sqlescape($username)."')");
-	if (is_string($result)) {
-		$FormErrors[count($FormErrors)] = "Could not SELECT from the vtcal_adminuser table. Does the user specified in the Database Connection String have access?<br/>Error: <code>" . htmlentities($result) . "</code>";
+	
+	// Return an error message if the SELECT failed.
+	if (is_string($result)) return "Could not SELECT from the vtcal_adminuser table. If you have not created the DB, you will need to <a href='upgradedb.php'>Install or Upgrade Database (MySQL Only)</a> first.<br/>Error: <code>" . htmlentities($result) . "</code>";
+	
+	$record =& $result->fetchRow(DB_FETCHMODE_ASSOC,0);
+	if ($record['idcount'] != "0") {
+		// Return an error message if the user already exists.
+		return "'" . htmlentities($username) . "' is already an admin user.";
+		
+		// - or -
+		
+		// Return true since the user already exists.
+		//return true;
 	}
-	else {
-		$record =& $result->fetchRow(DB_FETCHMODE_ASSOC,0);
-		if ($record['idcount'] == "0") {
-			// Insert the account if it does not exist.
-			$result =& DBQuery("INSERT INTO vtcal_adminuser (id) VALUES ('".sqlescape($username)."')");
-			if (is_string($result)) {
-				$FormErrors[count($FormErrors)] = "Could not assign '" . htmlentities($username) . "' as an admin user.<br/>Error: <code>" . htmlentities($result) . "</code>";
-			}
-		}
-		else {
-			$FormErrors[count($FormErrors)] = "'" . htmlentities($username) . "' is already an admin user.";
-		}
-	}
+	
+	// Insert the account if it does not exist.
+	$result =& DBQuery("INSERT INTO vtcal_adminuser (id) VALUES ('".sqlescape($username)."')");
+	
+	// Return an error message if the INSERT failed.
+	if (is_string($result)) return "Could not assign '" . htmlentities($username) . "' as an admin user.<br/>Error: <code>" . htmlentities($result) . "</code>";
+	
+	// Return that the user was added successfully.
+	return true;
 }
 
 function AddUser($username, $password) {
-	global $FormErrors;
 	
 	// Check that the account does not already exist
 	$result =& DBQuery("SELECT count(*) as idcount FROM vtcal_user WHERE (id='".sqlescape($username)."')");
-	if (is_string($result)) {
-		$FormErrors[count($FormErrors)] = "Could not SELECT from the vtcal_user table. Does the user specified in the Database Connection String have access?<br/>Error: <code>" . htmlentities($result) . "</code>";
+	
+	// Return an error message if the SELECT failed.
+	if (is_string($result)) return "Could not SELECT from the vtcal_user table. If you have not created the DB, you will need to <a href='upgradedb.php'>Install or Upgrade Database (MySQL Only)</a> first.<br/>Error: <code>" . htmlentities($result) . "</code>";
+	
+	$record =& $result->fetchRow(DB_FETCHMODE_ASSOC,0);
+	if ($record['idcount'] != "0") {
+		// Return an error message if the user already exists.
+		return "The '" . htmlentities($username) . "' account already exists.";
+		
+		// - or -
+		
+		// Return true since the user already exists.
+		//return true;
 	}
-	else {
-		$record =& $result->fetchRow(DB_FETCHMODE_ASSOC,0);
-		if ($record['idcount'] == "0") {
-			// Insert the account if it does not exist.
-			$result =& DBQuery("INSERT INTO vtcal_user (id, password, email) VALUES ('".sqlescape($username)."','".sqlescape(crypt($password))."','')");
-			if (is_string($result)) {
-				$FormErrors[count($FormErrors)] = "Could not create the '" . htmlentities($username) . "' account.<br/>Error: <code>" . htmlentities($result) . "</code>";
-			}
-		}
-		else {
-			$FormErrors[count($FormErrors)] = "The '" . htmlentities($username) . "' account already exists.";
-		}
-	}
+	
+	// Insert the account if it does not exist.
+	$result =& DBQuery("INSERT INTO vtcal_user (id, password, email) VALUES ('".sqlescape($username)."','".sqlescape(crypt($password))."','')");
+	
+	// Return an error message if the INSERT failed.
+	if (is_string($result)) return "Could not create the '" . htmlentities($username) . "' account.<br/>Error: <code>" . htmlentities($result) . "</code>";
+	
+	// Return that the user was added successfully.
+	return true;
 }
 
 ?>

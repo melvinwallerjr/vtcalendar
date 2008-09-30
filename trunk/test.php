9a3ecf30cbf2470e7972710111ea696a70a05a45
amekkawi <amekkawi@12b15db6-9489-4049-b62a-67b714a0ed3f>
2008-09-30 04:26:56 +0000

    Changed DrawCalendar() in test.php to look more like the little calendar in VTCalendar.

--- trunk/test.php
+++ trunk/test.php
@@ -8,15 +8,31 @@
 	border-collapse: collapse;
 }
 .CalTable td {
-	font-family: "Courier New", Courier, mono;
-	font-size: 13px;
-	border: 1px solid #999999;
+	font-family: Arial;
+	font-size: 11px;
+	/*border: 1px solid #999999;*/
+	padding: 0;
 }
-.CalTable .MonthTitle {
-	background-color: #DDDDDD;
+.CalTable a {
+	display: block;
+	padding: 2px;
+	text-decoration: none;
+	color: #0000FF;
 }
-.CalTable .OtherMonthDays {
-	background-color: #EEEEEE;
+.CalTable a:hover {
+	text-decoration: underline;
+}
+.CalTable .MonthTitleRow td {
+	font-family: Verdana, Arial;
+	font-size: 16px;
+	font-weight: bold;
+	background-color: #DDDDFF;
+}
+.CalTable .DOWRow td, .CalTable .WeekCell {
+	/*background-color: #CCCCCC;*/
+}
+.CalTable .OtherMonthDays a {
+	color: #BBBBBB;
 }
 </style>
 </head>
@@ -38,12 +54,14 @@ echo "<pre>\n";
 echo "</pre>";
 
 echo "<table class='CalTable' border='0' cellpadding='2' cellspacing='0'>\n";
-DrawCalendar(2008, 1);
-DrawCalendar(2008, 2);
-DrawCalendar(2008, 3);
+$now = new vtDate();
+$now->setDay(1);
+DrawCalendar($now->getYear(), $now->getMonth());
+$now->add("1 month"); DrawCalendar($now->getYear(), $now->getMonth());
+$now->add("1 month"); DrawCalendar($now->getYear(), $now->getMonth());
 echo "</table>";
 
-function DrawCalendar($year, $month, $hideOtherMonths = true, $noTableTags = true) {
+function DrawCalendar($year, $month, $hideOtherMonths = false, $showDOW = true, $noTableTags = true) {
 	$dowstart = 0;
 	
 	// Determine the first and last day of the month
@@ -65,35 +83,39 @@ function DrawCalendar($year, $month, $hideOtherMonths = true, $noTableTags = tru
 	//echo $displayDays . "\n";
 	
 	if (!$noTableTags)
-		echo "<table class='CalTable' border='0' cellpadding='2' cellspacing='0'>\n";
+		echo '<table class="CalTable" border="0" cellpadding="2" cellspacing="0">' . "\n";
 	
-	echo '<tr><td align="center" colspan="7" class="MonthTitle">' . $firstDay->format("%F") . '</td></tr>';
+	echo '<tr class="MonthTitleRow"><td align="center" colspan="8"><a href="#">' . $firstDay->format("%F") . "</a></td></tr>\n";
 	
 	// Week Day Names
-	echo "<tr>\n";
-	for ($i = 0; $i < 7; $i++) {
-		echo "<td align='center'>" . vtDate::format("%D", strtotime("+".$i." days", $firstDisplayDay->getEpoch())) . "</td>\n";
+	if ($showDOW) {
+		echo "<tr class='DOWRow'>\n";
+		echo "<td>&nbsp;</td>";
+		for ($i = 0; $i < 7; $i++) {
+			echo '<td align="center"><a href="#">' . substr(vtDate::format("%D", strtotime("+".$i." days", $firstDisplayDay->getEpoch())), 0, 1) . "</a></td>\n";
+		}
+		echo "</tr>\n";
 	}
-	echo "</tr>\n";
 	
 	$currentDay =& $firstDisplayDay->copy();
 	for ($i = 0; $i <= $displayDays; $i++) {
 		if ($i % 7 == 0) {
-			echo "<tr>";
+			echo '<tr class="EventRow Row"' . $i . '">';
+			echo '<td class="WeekCell"><a href="#">Wk&gt;</a></td>';
 		}
 		
 		if ($currentDay->getMonth() != $firstDay->getMonth()) {
-			echo "<td align='center' class='OtherMonthDays'>";
+			echo '<td align="center" class="OtherMonthDays">';
 		}
 		else {
-			echo "<td align='center'>";
+			echo '<td align="center">';
 		}
 		
 		if ($hideOtherMonths && ($currentDay->getYear() != $year || $currentDay->getMonth() != $month)) {
 			echo "&nbsp;";
 		}
 		else {
-			echo $currentDay->format("%j");
+			echo '<a href="#">' . $currentDay->format("%j") . '</a>';
 		}
 		
 		echo "</td>";

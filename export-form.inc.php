bd7f8ed5ae9092c3d8b26eb5228046d00c41dc55
amekkawi <amekkawi@12b15db6-9489-4049-b62a-67b714a0ed3f>
2008-10-04 16:35:55 +0000

    export-form.inc.php: - Fixes to HTML - Implemented a drop-down box for the sponsor list, but decided to disable it pending review of which is better.

--- export-form.inc.php
+++ export-form.inc.php
@@ -26,54 +26,55 @@ pageheader(lang('export_events'), "");
 contentsection_begin(lang('export_events'));
 
 ?>
-<a target="newWindow" onclick="new_window(this.href); return false" 
+<p><a target="newWindow" onclick="new_window(this.href); return false" 
 	 href="helpexport.php"><img src="images/help.gif" width="16" height="16" alt="" border="0"> 
-	 <?php echo lang('how_to_export_events'); ?></a>
-<br>
-<br>
+	 <?php echo lang('how_to_export_events'); ?></a></p>
+
 <form method="get" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-<table border="0" cellspacing="0" cellpadding="0">
+<table border="0" cellspacing="0" cellpadding="6">
 	<tr>
 		<td class="bodytext" valign="top">
 			<strong><?php echo lang('output_format'); ?></strong>&nbsp;
 		</td>
-		<td>
-			<select name="type">
+		<td><select name="type">
 				<option value="xml">XML/VT Calendar</option>
 				<option value="ical">iCalendar</option>
 				<option value="rss">XML/RSS 0.91</option>
 				<option value="rss1_0">XML/RSS 1.0</option>
 				<option value="vxml">VoiceXML 2.0</option>
-			</select> <br>
-			<br>
-		</td>
+			</select></td>
 	</tr>
 	<TR>
 		<TD class="bodytext" valign="top">
 			<strong><?php echo lang('category'); ?>:</strong>
 		</TD>
 		<TD class="bodytext" valign="top">
-			<SELECT name="categoryid" size="1">
-<?php
-$result = DBQuery("SELECT * FROM vtcal_category WHERE calendarid='".sqlescape($_SESSION['CALENDAR_ID'])."'" ); 
-
-// print list with categories from the DB
-echo "<OPTION ";
-if (empty($categoryid) || $categoryid==0) {
-	echo "selected ";
-}
-echo "value=\"0\">all</OPTION>\n";
-
-for ($i=0; $i<$result->numRows(); $i++) {
-	$category = $result->fetchRow(DB_FETCHMODE_ASSOC,$i);
-	echo "<OPTION ";
-	if (!empty($categoryid) && $categoryid==$category['id']) { echo "selected "; }
-	echo "value=\"",htmlentities($category['id']),"\">",htmlentities($category['name']),"</OPTION>\n";
-}
-?>
-			</SELECT><br>
-			<br>
-		</TD>
+				<?php
+				$result =& DBQuery("SELECT * FROM vtcal_category WHERE calendarid='".sqlescape($_SESSION['CALENDAR_ID'])."'" ); 
+				
+				if (is_string($result)) {
+					// TODO: Need DB failed message.
+				}
+				else {
+					echo '<SELECT name="categoryid" size="1">';
+					
+					// print list with categories from the DB
+					echo "<OPTION ";
+					if (empty($categoryid) || $categoryid==0) {
+						echo "selected ";
+					}
+					echo "value=\"0\">all</OPTION>\n";
+					
+					for ($i=0; $i<$result->numRows(); $i++) {
+						$category = $result->fetchRow(DB_FETCHMODE_ASSOC,$i);
+						echo "<OPTION ";
+						if (!empty($categoryid) && $categoryid==$category['id']) { echo "selected "; }
+						echo "value=\"",htmlentities($category['id']),"\">",htmlentities($category['name']),"</OPTION>\n";
+					}
+					
+					echo '</SELECT>';
+				}
+				?></TD>
 	</TR>
 	<TR>
 		<TD class="bodytext" valign="top">
@@ -86,12 +87,33 @@ for ($i=0; $i<$result->numRows(); $i++) {
 		echo '<input type="radio" name="sponsortype" value="self"> ',$_SESSION["AUTH_SPONSORNAME"],"<br>\n";
 	}
 ?>
-				<input type="radio" name="sponsortype" value="specific"> <?php echo lang('specific_sponsor'); ?>
-				<INPUT type="text" size="28" maxlength="<?php echo constSpecificsponsorMaxLength; ?>" name="specificsponsor" value="">
-			<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		<i><?php echo lang('specific_sponsor_example'); ?></i><BR>
-			<br>
-		</TD>
+				<input type="radio" name="sponsortype" value="specific"> <?php echo lang('specific_sponsor'); ?>:
+				
+				<?php
+				// TODO: Is having a drop-down list or a %x% search better?
+				//$result =& DBQuery("SELECT count(*) as sponsorcount, displayedsponsor FROM vtcal_event_public WHERE calendarid='". sqlescape($_SESSION['CALENDAR_ID']) ."' GROUP BY displayedsponsor ORDER BY displayedsponsor");
+				$result = "disabled";
+				
+				if (is_string($result)) {
+					?><INPUT type="text" size="28" maxlength="<?php echo constSpecificsponsorMaxLength; ?>" name="specificsponsor" value=""><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <i><?php echo lang('specific_sponsor_example'); ?></i><?php
+				}
+				else {
+					echo '<select name="specificsponsor" id="specificsponsor">';
+					
+					for ($i=0; $i<$result->numRows(); $i++) {
+						$sponsor = $result->fetchRow(DB_FETCHMODE_ASSOC, $i);
+						if ($sponsor['displayedsponsor'] == "") {
+							echo '<option value="">&lt;&lt;Events with no sponsor. Records: '. htmlentities($sponsor['sponsorcount']) .'&gt;&gt;</option>';
+						}
+						else {
+							echo '<option value="'. htmlentities($sponsor['displayedsponsor']) .'">'. htmlentities($sponsor['displayedsponsor']) .' (Records: '. htmlentities($sponsor['sponsorcount']) .')</option>';
+						}
+					}
+					
+					echo '</select>';
+				}
+				?>
+			</TD>
 	</TR>
 	<TR>
 		<TD class="bodytext" valign="top">
@@ -179,12 +201,11 @@ for ($i=date("Y", NOW)-1; $i<=date("Y", NOW)+3; $i++) {
 		</TR>
 	</TABLE>
 </table>
-<br>
 
-<?php echo lang('export_message'); ?><br>
-<br>
-<input type="submit" name="startexport" value="<?php echo lang('ok_button_text'); ?>">
-<input type="submit" name="cancel" value="<?php echo lang('cancel_button_text'); ?>">
+<p><?php echo lang('export_message'); ?></p>
+
+<p><input type="submit" name="startexport" value="<?php echo lang('ok_button_text'); ?>">
+<input type="submit" name="cancel" value="<?php echo lang('cancel_button_text'); ?>"></p>
 </form>
 
 <?php    

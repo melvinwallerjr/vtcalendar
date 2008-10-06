c95d77a5b76a3b730ecb8ff0255508aad7ef23ac
amekkawi <amekkawi@12b15db6-9489-4049-b62a-67b714a0ed3f>
2008-10-06 19:19:21 +0000

    - Created script to generate config validation and made small updates to dev/configs.xml to support this.

--- trunk/config-validation.inc.php
+++ trunk/config-validation.inc.php
@@ -1,6 +1,61 @@
 <?php
 // =====================================
-// Config Validation
+// GENERATED Config Validation
+// =====================================
+
+if (!is_string(TITLEPREFIX)) exit('TITLEPREFIX must be a string.');
+if (!is_string(TITLESUFFIX)) exit('TITLESUFFIX must be a string.');
+if (!is_string(LANGUAGE)) exit('LANGUAGE must be a string.');
+if (LANGUAGE == '') exit('LANGUAGE cannot be an empty string.');
+if (!is_string(DATABASE)) exit('DATABASE must be a string.');
+if (DATABASE == '') exit('DATABASE cannot be an empty string.');
+if (!is_string(SQLLOGFILE)) exit('SQLLOGFILE must be a string.');
+if (!is_string(REGEXVALIDUSERID)) exit('REGEXVALIDUSERID must be a string.');
+if (REGEXVALIDUSERID == '') exit('REGEXVALIDUSERID cannot be an empty string.');
+if (!is_bool(AUTH_DB)) exit('AUTH_DB must be a boolean true or false. Make sure it is not enclosed in quotes.');
+if (!is_string(AUTH_DB_USER_PREFIX)) exit('AUTH_DB_USER_PREFIX must be a string.');
+if (!is_string(AUTH_DB_NOTICE)) exit('AUTH_DB_NOTICE must be a string.');
+if (!is_bool(AUTH_LDAP)) exit('AUTH_LDAP must be a boolean true or false. Make sure it is not enclosed in quotes.');
+if (!is_string(LDAP_HOST)) exit('LDAP_HOST must be a string.');
+if (LDAP_HOST == '') exit('LDAP_HOST cannot be an empty string.');
+if (!is_numeric(LDAP_PORT)) exit('LDAP_PORT must be an numeric.');
+if (!is_string(LDAP_USERFIELD)) exit('LDAP_USERFIELD must be a string.');
+if (LDAP_USERFIELD == '') exit('LDAP_USERFIELD cannot be an empty string.');
+if (!is_string(LDAP_BASE_DN)) exit('LDAP_BASE_DN must be a string.');
+if (LDAP_BASE_DN == '') exit('LDAP_BASE_DN cannot be an empty string.');
+if (!is_string(LDAP_SEARCH_FILTER)) exit('LDAP_SEARCH_FILTER must be a string.');
+if (!is_string(LDAP_BIND_USER)) exit('LDAP_BIND_USER must be a string.');
+if (LDAP_BIND_USER == '') exit('LDAP_BIND_USER cannot be an empty string.');
+if (!is_string(LDAP_BIND_PASSWORD)) exit('LDAP_BIND_PASSWORD must be a string.');
+if (LDAP_BIND_PASSWORD == '') exit('LDAP_BIND_PASSWORD cannot be an empty string.');
+if (!is_bool(AUTH_HTTP)) exit('AUTH_HTTP must be a boolean true or false. Make sure it is not enclosed in quotes.');
+if (!is_string(AUTH_HTTP_URL)) exit('AUTH_HTTP_URL must be a string.');
+if (AUTH_HTTP_URL == '') exit('AUTH_HTTP_URL cannot be an empty string.');
+if (!is_string(BASEPATH)) exit('BASEPATH must be a string.');
+if (!is_string(BASEDOMAIN)) exit('BASEDOMAIN must be a string.');
+if (!is_string(BASEURL)) exit('BASEURL must be a string.');
+if (BASEURL == '') exit('BASEURL cannot be an empty string.');
+if (!is_string(SECUREBASEURL)) exit('SECUREBASEURL must be a string.');
+if (!is_numeric(TIMEZONE_OFFSET)) exit('TIMEZONE_OFFSET must be an numeric.');
+if (!is_numeric(WEEK_STARTING_DAY)) exit('WEEK_STARTING_DAY must be an numeric.');
+if (!is_bool(USE_AMPM)) exit('USE_AMPM must be a boolean true or false. Make sure it is not enclosed in quotes.');
+if (!is_string(COLUMNSIDE)) exit('COLUMNSIDE must be a string.');
+if (COLUMNSIDE == '') exit('COLUMNSIDE cannot be an empty string.');
+if (!is_bool(SHOW_UPCOMING_TAB)) exit('SHOW_UPCOMING_TAB must be a boolean true or false. Make sure it is not enclosed in quotes.');
+if (!is_numeric(MAX_UPCOMING_EVENTS)) exit('MAX_UPCOMING_EVENTS must be an numeric.');
+if (!is_bool(SHOW_MONTH_OVERLAP)) exit('SHOW_MONTH_OVERLAP must be a boolean true or false. Make sure it is not enclosed in quotes.');
+if (!is_bool(AUTH_HTTP_CACHE)) exit('AUTH_HTTP_CACHE must be a boolean true or false. Make sure it is not enclosed in quotes.');
+if (!is_numeric(AUTH_HTTP_CACHE_EXPIRATIONDAYS)) exit('AUTH_HTTP_CACHE_EXPIRATIONDAYS must be an numeric.');
+if (!is_numeric(MAX_CACHESIZE_CATEGORYNAME)) exit('MAX_CACHESIZE_CATEGORYNAME must be an numeric.');
+if (!is_bool(CACHE_ICS)) exit('CACHE_ICS must be a boolean true or false. Make sure it is not enclosed in quotes.');
+if (!is_string(EXPORT_PATH)) exit('EXPORT_PATH must be a string.');
+if (EXPORT_PATH == '') exit('EXPORT_PATH cannot be an empty string.');
+if (!is_numeric(MAX_EXPORT_EVENTS)) exit('MAX_EXPORT_EVENTS must be an numeric.');
+if (!is_numeric(EXPORT_CACHE_MINUTES)) exit('EXPORT_CACHE_MINUTES must be an numeric.');
+if (!is_bool(PUBLIC_EXPORT_VTCALXML)) exit('PUBLIC_EXPORT_VTCALXML must be a boolean true or false. Make sure it is not enclosed in quotes.');
+
+// =====================================
+// Manual Config Validation
 // =====================================
 
 // Check for an LDAP function if AUTH_LDAP is true.
